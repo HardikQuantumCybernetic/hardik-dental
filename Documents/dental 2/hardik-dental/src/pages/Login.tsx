1bd5b9321bff4247f33c6b26a02315e0f4ca5eff
Hardik Dattatray Jadhav <165682674+HardikQuantumCybernetic@users.noreply.github.com>
2026-02-06 07:31:58 +0530

    Delete Documents directory

--- Documents/dental 2/hardik-dental/src/pages/Login.tsx
+++
@@ -1,7 +0,0 @@
-import ClerkLoginForm from "@/components/auth/ClerkLoginForm";
-
-const Login = () => {
-  return <ClerkLoginForm />;
-};
-
-export default Login;
--- Documents/dental 2/hardik-dental/src/pages/SignUp.tsx
+++
@@ -1,7 +0,0 @@
-import ClerkSignUpForm from "@/components/auth/ClerkSignUpForm";
-
-const SignUp = () => {
-  return <ClerkSignUpForm />;
-};
-
-export default SignUp;
